305453d93b2bd47a145fe70d7715c4aa77a55f22
DavidFulton1 <fultondavids@gmail.com>
2022-07-06 14:23:58 -0500

    Add project files.

---
+++ OperatorExcercise/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExcercise
+{
+    public class Operator_Class
+    {
+        public static double AreaOfCircle()
+        {
+            Console.WriteLine("please enter the radius");
+            var r = double.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine(($"The area of a circle with this radius is {area}")); 
+            return area;
+        }
+    }
+}
+
---
+++ OperatorExcercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using OperatorExcercise;
+
+int a = 17;
+int b = 4;
+int addition = a + b;
+Console.WriteLine($"17 + 4 ={addition}");
+
+int subtraction = a - b;
+Console.WriteLine($"17 - 4 = {subtraction}");
+
+int multiplication=a * b;
+Console.WriteLine($"17 x 4 = {multiplication}");
+
+int quotient = a / b;
+int remainder= a % b;
+Console.WriteLine( $" {a} /{b} is {quotient} with ramainder {remainder}.");
+
+ Operator_Class.AreaOfCircle();
+
